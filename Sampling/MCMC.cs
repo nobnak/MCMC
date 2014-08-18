229070adbdd02e8f1d039f9393db5ad1ac0fa0a0
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-08-18 16:28:58 +0900

    Squashed 'Assets/nobnak/' changes from 3090701..ae9b816
    ae9b816 no message
    5709109 no message
    
    git-subtree-dir: Assets/nobnak
    git-subtree-split: ae9b816b8d2eaf1620ed8e4a5c3d80774ad21211

---
+++ Sampling/MCMC.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace nobnak.Sampling {
+
+	public class MCMC {
+		public Texture2D ProbTex { get; private set; }
+		public float StdDev { get; private set; }
+		public float Aspect { get; private set; }
+		public float Height { get; private set; }
+		public float Epsilon { get; private set; }
+
+		private Vector2 _curr;
+		private float _currDensity;
+		private Vector2 _stddevAspect;
+
+		public MCMC(Texture2D probTex, float stddev, float aspect) : this(probTex, stddev, aspect, 1f, 1e-6f) {}
+		public MCMC(Texture2D probTex, float stddev, float aspect, float height, float epsilon) {
+			this.ProbTex = probTex;
+			this.Aspect = aspect;
+			this.Height = height;
+			this.Epsilon = epsilon;
+			this.StdDev = stddev;
+		}
+
+		public IEnumerable<Vector2> Sequence(int nInitialize, int limit) {
+			return Sequence(nInitialize, limit, 0);
+		}
+		public IEnumerable<Vector2> Sequence(int nInitialize, int limit, int nSkip) {
+			_curr = new Vector2(Random.value, Random.value);
+			_currDensity = Density(_curr);
+			_stddevAspect = new Vector2(StdDev, StdDev / Aspect);
+
+			for (var i = 0; i < nInitialize; i++)
+				Next();
+
+			for (var i = 0; i < limit; i++) {
+				for (var j = 0; j < nSkip; j++)
+					Next();
+				yield return _curr;
+				Next ();
+			}
+		}
+		
+		void Next() {
+			var next = Vector2.Scale(_stddevAspect, BoxMuller.Gaussian()) + _curr;
+			next.x -= Mathf.Floor(next.x);
+			next.y -= Mathf.Floor(next.y);
+			
+			var densityNext = Density(next);
+			if (Mathf.Min(1f, densityNext / _currDensity) >= Random.value) {
+				_curr = next;
+				_currDensity = densityNext;
+			}
+		}
+		float Density(Vector2 curr) {
+			return Height * ProbTex.GetPixelBilinear(curr.x, curr.y).r + Epsilon;
+		}
+
+	}
+}
